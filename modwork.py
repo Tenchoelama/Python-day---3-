ea19e94b9b6e55c2a807c84207ff5936156db2b8
Tenchoelama <113133609+Tenchoelama@users.noreply.github.com>
2023-01-05 17:04:05 -0600

    Add files via upload

---
+++ modwork.py
@@ -0,0 +1,22 @@
+import math 
+
+#Question 1
+
+
+def calculate_square_footage(length,width):
+    
+    area = (length * width) 
+    return area
+
+
+
+
+#Question 2
+
+def circum_of_a_circle(r):
+    
+    C = 2 * math.pi * r
+    return C
+
+
+
